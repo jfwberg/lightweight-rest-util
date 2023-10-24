5fcbd0e04781395a1ee1e1f5b34da7fe3d91a58b
Justus van den Berg <jfwberg@gmail.com>
2023-10-24 09:26:58 +0100

    Updated permission set updated package version

--- scripts/02_package_create_managed.bat
+++ scripts/02_package_create_managed.bat
@@ -11,7 +11,7 @@ SET packagePath=force-app/package
 
 REM Package Config
 SET packageId=0HoP30000000001KAA
-SET packageVersionId=04tP30000007FOvIAM
+SET packageVersionId=04tP30000007sN3IAI
 
 REM Create package
 sf package create --name "%packageName%" --description "%packageDescription%" --package-type "%packageType%" --path "%packagePath%" --target-dev-hub %devHub%
